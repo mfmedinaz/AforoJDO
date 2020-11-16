6ebfbe0979470662cf1f4ff69446fe76ad981b11
Mario Medina <mf.medinaz@uniandes.edu.co>
2020-11-16 15:10:50 -0500

    jj

--- doc/Iteracion3RFC.sql
+++ doc/Iteracion3RFC.sql
@@ -2,12 +2,30 @@
 --RFC7 - Analizar la operación de AFOROCC
 
 --Hora mayor afluencia
+--Primero, buscamos cuántos Espacios de un determinado tipo_establecimiento fueron visitados en un determinado tiempo (para después comparar estos valores y ver cuál hora es la de mayor afluencia en AforoJDO)
 SELECT COUNT(Espacio.id)
 FROM VISITANTE
 INNER JOIN VISITA
 ON VISITANTE.id = VISITA.visitante
 INNER JOIN ESPACIO
-ON VISITA.lector = ESPACIO.lector;  
+ON VISITA.lector = ESPACIO.lector
+INNER JOIN LOCAL_COMERCIAL
+ON LOCAL_COMERCIAL.id_espacio = ESPACIO.id
+WHERE local_comercial.tipo_establecimiento = 'TIENDA' AND hora_inicial BETWEEN TO_DATE('2020-11-02-23:59:59', 'YYYY-MM-DD-HH24:MI:SS')-11 AND TO_DATE('2020-11-02-23:59:59', 'YYYY-MM-DD-HH24:MI:SS'); 
+
+--RFC8: ENCONTRAR LOS CLIENTES FRECUENTES
+SELECT VISITANTE.*
+FROM VISITANTE
+INNER JOIN VISITA
+ON VISITANTE.id = VISITA.visitante
+INNER JOIN ESPACIO
+ON VISITA.lector = ESPACIO.lector
+INNER JOIN LOCAL_COMERCIAL
+ON LOCAL_COMERCIAL.id_espacio = ESPACIO.id
+WHERE ESPACIO.id = 7 AND VISITANTE.tipo_visitante = 'CONSUMIDOR' AND hora_inicial BETWEEN TO_DATE('2020-11-02-23:59:59', 'YYYY-MM-DD-HH24:MI:SS')-11 AND TO_DATE('2020-11-02-23:59:59', 'YYYY-MM-DD-HH24:MI:SS');
+
+
+
 
 
 --RFC9: Encontrar los visitantes que estuvieron en contacto con otro determinado visitante
@@ -22,9 +40,7 @@ INNER JOIN ESPACIO
 ON VISITA.lector = ESPACIO.lector
 WHERE VISITANTE.id = 1 AND hora_inicial BETWEEN TO_DATE('2020-11-02-23:59:59', 'YYYY-MM-DD-HH24:MI:SS')-11 AND TO_DATE('2020-11-02-23:59:59', 'YYYY-MM-DD-HH24:MI:SS');  
 
-
-
-
+-- luego esto, se buscan los visitantes que comparten la visita
 SELECT VISITANTE.*
 FROM VISITANTE
 INNER JOIN VISITA
